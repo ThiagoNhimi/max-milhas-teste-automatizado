c7972fdd66cbc7369dd0c5f54e84dfa84977497a
Thiago Martins <114078535+ThiagoNhimi@users.noreply.github.com>
2024-03-11 18:30:23 -0300

    Add files via upload

---
+++ cypress/e2e/login/loginMaxMilhas.cy.js
@@ -0,0 +1,10 @@
+/// <reference types="cypress"/>
+
+
+describe('Importando Realizado e Comparando Valores',()=>{ 
+    it('Login preenchendo os campos de forma errônea',()=>{
+        cy.visit('www.maxmilhas.com.br/')
+      })
+
+  
+});
